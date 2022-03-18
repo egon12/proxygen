2596838b5a23ed360e50b637b677b68cf8264fb4
Egon Firman <egon.firman@tokopedia.com>
2022-03-18 17:38:17 +0700

    using tools/imports for fix imports

--- file.go
+++ file.go
@@ -4,6 +4,8 @@ import (
 	"os"
 	"os/exec"
 	"strings"
+
+	"golang.org/x/tools/imports"
 )
 
 func GenerateTracerFileName(filename string) string {
@@ -12,10 +14,35 @@ func GenerateTracerFileName(filename string) string {
 
 // FixImports will fix and formattion any missing error in the generated code
 func FixImports(filename string) error {
+	//return fixImportsOld(filename)
+	return fixImportsNew(filename)
+}
+
+func fixImportsOld(filename string) error {
 	content, err := exec.Command("goimports", filename).CombinedOutput()
 	if err != nil {
 		return err
 	}
+
+	output, err := os.Create(filename)
+	defer func() {
+		_ = output.Close()
+	}()
+
+	if err != nil {
+		return err
+	}
+
+	_, err = output.Write(content)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func fixImportsNew(filename string) error {
+	content, err := imports.Process(filename, nil, nil)
 
 	output, err := os.Create(filename)
 	defer func() {
